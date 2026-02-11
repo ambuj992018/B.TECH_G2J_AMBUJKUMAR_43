5f027090c9b1dc4836259d6c6c8af2159b5a62b5
ambuj992018 <ambujkumar119419@gmail.com>
2026-02-11 12:26:29 +0530

    Add files via upload

---
+++ p11.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main () {
+
+    float a,b,c;
+ printf("Enter num1 = ");
+ scanf("%f",&a);
+ printf("Enter num2 = ");
+ scanf("%f",&b);
+printf("Enter the operator = ");
+
+
+return 0;
+}
