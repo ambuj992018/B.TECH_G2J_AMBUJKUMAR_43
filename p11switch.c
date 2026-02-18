eafecc614d2cf1c5dbe8bfe07af69328833c562e
ambuj992018 <ambujkumar119419@gmail.com>
2026-02-18 10:55:31 +0530

    Add files via upload

---
+++ p11switch.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main() {
+    int a,b;
+    char op;
+    printf("Enter num1 = ");
+    scanf("%d",&a);
+    printf("Enter num2 =");
+    scanf("%d",&b);
+    printf("Enter the operator(+,-,*,/): ");
+    scanf(" %c",&op);
+
+    switch(op){
+        case '+':
+        printf("sum = %d",a+b);
+        break;
+        case '-' :
+        printf("subtraction = %d",a-b);
+        break;
+        case '*' :
+        printf("multiplication = %d",a*b);
+        break;
+        case '/' :
+        if(b>0)
+        printf("division = %f",(float)a/b);
+        else 
+        printf("can't be performed");
+
+        default :
+        printf("error");
+
+    }
+     return 0;
+}
